dac9b78a787622b81b4f0fdc3653b2f1d8b779c9
anna mendoza <anna@curvecommunications.biz>
2014-01-01 23:05:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhartlBookThirdAppAgain::Application.config.secret_token = 'b617b664e1a716e37042808cd83fab933f6c6df000d6fa296a18537d26f1c3fdf9f63e85d48b2123333ca53019190a594ee7f0bb5af4f76841acb4d102630803'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MhartlBookThirdAppAgain::Application.config.session_store :cookie_store, key: '_mhartl-book-third-app-again_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MhartlBookThirdAppAgain::Application.config.session_store :active_record_store
